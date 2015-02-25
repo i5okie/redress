c12ec7238c52cadf028688d531d3e07c23517a05
i5okie <ipzent@gmail.com>
2015-02-24 23:21:06 -0800

    integrate recaptcha with devise

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,7 @@
 Recaptcha.configure do |config|
   config.public_key  = '6LeEnwITAAAAANmfDBqpWVRRId9xswR91ub57YON'
   config.private_key = '6LeEnwITAAAAAKrkzPaUmoBM34Z2Hao__TbA-O-M'
+  config.use_ssl_by_default = true
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
   # Uncomment if you want to use the newer version of the API,
